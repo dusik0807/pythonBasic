c0d7ad1251345fca08f1c252dc274a6472322e25
dusik0807 <90747125+dusik0807@users.noreply.github.com>
2021-09-29 15:53:40 +0900

    Create main.py

---
+++ 2강/main.py
@@ -0,0 +1,28 @@
+print("안녕하세요!오늘은 '수요일'입니다")
+
+print("월요일,화요일,수요일,목요일,금요일")
+
+A=10000
+B=25000
+C=30000
+D=A+B+C
+print(D)
+print(D/3)
+
+print("총 금액:",D,"평균 금액 : ", D/3)
+
+a="Life is too short, You need Python"
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[0]+a[1]+a[2]+a[3])
+print(a[-6]+a[-5]+a[-4]+a[-3]+a[-2]+a[-1])
+print(a[0:4])
+print(a[27:34])
+print(a[5:7])
+print(a[23:28])
+print(a[19:-7])
+a="20210929Rain"
+print(a[:8])
+print(a[8:])
