ba769169dd2eaac449329e74b3e72a24eba912f4
dusik0807 <90747125+dusik0807@users.noreply.github.com>
2021-10-06 15:52:38 +0900

    Create main1.py

---
+++ 3강/main1.py
@@ -0,0 +1,37 @@
+
+print('Hello, world!')
+print('안녕하세요')
+print("오늘은","수요ㅇ;\\")
+age=12
+print(age)
+공책=2
+필통=1
+교과서=4
+print(공책,필통,교과서)
+age=3
+print(age)
+string='''오늘 날씨는 흐리다.
+나는 오늘 코딩을 합니다.'''
+print(string)
+print('''안녕하세요!저는 12살입니다.''')
+num=15
+string="15"
+print(num)
+print(string)
+num=2021
+string="2021"
+print(num)
+print(string)
+print('''\   /\\
+ ) ( ^)
+(  /  )
+ \(__)|''')
+
+
+
+
+a="오늘 날씨는 "
+s="바람이 분다"
+print(a+s)
+a="파이썬 "
+print(a*2)
