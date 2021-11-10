79ae326f40e71c2b61f8e94c77efaf3f5e4ae691
dusik0807 <90747125+dusik0807@users.noreply.github.com>
2021-11-10 15:48:01 +0900

    Create main1.py

---
+++ 8강/main1.py
@@ -0,0 +1,89 @@
+#for number in range(1,21,1):
+  #  print(number)
+
+#for number in range(1,101,1):
+   # print(number)
+
+#max=int(input())
+#for number in range(0,max+1,1):
+ #   print(number)
+
+#print("숫자 두 개를 작은수부터 입력해주세요")
+#min=int(input())
+#max=int(input())
+#for number in range(min,max+1,1):
+ #   print(number)
+
+#for number in range(1,10):
+ #   string='{}*{}={}'.format(2,number,2*number)
+  #  print(string)
+
+#for number in range(1,10):
+ #   string='{}*{}={}'.format(9,number,9*number)
+  #  print(string)
+
+#단=9
+#for number in range(1,10):
+ #   string='{}*{}={}'.format(단,number,단*number)
+  #  print(string)
+
+#단=int(input('구구단을 출력합니다.몇 단인지 입력해주세요.'))
+#for number in range(1,10):
+ #   string='{}*{}={}'.format(단,number,단*number)
+  #  print(string)
+
+
+
+#for 단 in range(2,10):
+ #   for number in range(1,10):
+  #      string='{}*{}={}'.format(단, number,단* number)
+   #     print(string)
+
+
+#for 단 in range(2,10):
+#    print('{}단 시작'.format(단))
+ #      string='{}*{}={}'.format(단, number,단* number)
+     #   print(string)
+   # print('{}단 종료'.format(단))
+
+#시작단=int(input('구구단 시작 단을 입력하세요(1~9):'))
+# 끝단=int(input('구구단 끝 단을 입력하세요(1~9):'))
+
+#a=0
+#while a < 10:
+ #   a=a+1
+  #  if a % 2 == 0:
+   ##     continue
+    #print(a)
+
+#a=0
+#while a < 10:
+ #   a = a + 1
+  #  if a == 5:
+  #    print('{}입니다.'.format(a))
+ #     break
+  #  print(a)
+  
+#a=3
+#while a < 7:
+#    print(a)
+ #   print("-------")
+#    a=a + 1
+
+#for i in range(1,11):
+ #   print(i)
+
+#for i in range(10):
+ #   print(i)
+
+#for i in range(10,0,-1):
+ #   print(i)
+
+
+a=0
+while a < 1000:
+    a=a + 1
+    if a % 3 !=0:
+        continue
+    print(a)
+
